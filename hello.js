7e3a763c328c1d4797c8b8e91b28512beb705261
Matthew Knox <mrkno@users.noreply.github.com>
2017-05-04 16:21:48 +1200

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 	The main entry point of the module. This will be called whenever a match is found via kassy.json.
 	In this basic example it simply replies with "hello world".
 */
-exports.run = function(api, event) {
+exports.run = (api, event) => {
 	api.sendMessage("hello world", event.thread_id);
 };
